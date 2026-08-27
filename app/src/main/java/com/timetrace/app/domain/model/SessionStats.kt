19d4954b279beff9fb1ae2eb222cf12815ea3e9a
azarx018 <zharr188@gmail.com>
2026-08-27 18:07:53 +0700

    ijoplis

--- app/src/main/java/com/timetrace/app/domain/model/SessionStats.kt
+++
@@ -1,9 +0,0 @@
-package com.timetrace.app.domain.model
-
-/** Aggregated session statistics for one app on one day (brief section 6). */
-data class SessionStats(
-    val sessions: List<UsageSession>,
-    val totalDurationMillis: Long,
-    val averageSessionMillis: Long,
-    val longestSessionMillis: Long
-)
